6a45acaf45c14c8bf3c95c1ee3688e2af83d7558
User <latapata1952@gmail.com>
2023-06-19 23:20:44 +0600

    project completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Home.css';
+import PlayButton from '../../components/PlayButton/PlayButton';
+import Reviews from '../../components/Reviews/Reviews';
+import MainReviews from '../../components/MainReviews/MainReviews';
 
 const Home = () => {
     return (
-        <div>
-            <p>Home</p>
-        </div>
+        <main className='main'>
+            <div className='wrapper'>
+                <section className='btn-section'>
+                    <PlayButton />
+                    <Reviews />
+                </section>
+                <MainReviews />
+            </div>
+        </main>
     );
 };
 
